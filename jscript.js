c85d84054da5bcdd8c2752aad18b6032831119fb
khalil <kalilovsky@gmail.com>
2022-01-12 09:40:40 +0100

    niveau 2

--- jscript.js
+++ jscript.js
@@ -1,10 +1,39 @@
 function verificationAge () {
-    let age = document.getElementById("ageInput").value;
-    if (age >= 18) {
-        alert ("Bienvenue dans notre site");
-    } else {
-        alert ("Désolé vous n'ếtes pas autorisé à entrer");
+    const today = new Date();
+    today.setHours(0,0,0);
+    
+    let age = new Date ;
+    age = document.getElementById("ageInput").value;
+    let dateEntered = new Date(age).getTime();
+    let differenceAge = new Date();
+    differenceAge = today - dateEntered;
+    differenceAge = (differenceAge/(31556952000));
+    console.log("Today = " + differenceAge);
+    console.log(differenceAge);
+    if (differenceAge >=18) {
+        alert("Bienvenue dans notre site");
+    }
+        else{
+            alert ("Vous n'êtes pas autoriser...."); 
     }
 
+    }  
+    document.addEventListener('keypress', (event) => {
+       
+        var code = event.code;
+        // Alert the key name and key code on keydown
+        if (code === "Enter") {
+            verificationAge();
+            
+        }
+        
+      }, false);
+      
 
-}
+    //   document.addEventListener('keypress', (event) => {
+    //     var name = event.key;
+    //     var code = event.code;
+    //     // Alert the key name and key code on keydown
+    //     alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    //   }, false);
+      
