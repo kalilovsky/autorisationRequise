d6da9efb5acc5ba7153cf19db284fbb9b55721b4
khalil <kalilovsky@gmail.com>
2022-01-12 09:27:20 +0100

    premier commit

---
+++ jscript.js
@@ -0,0 +1,10 @@
+function verificationAge () {
+    let age = document.getElementById("ageInput").value;
+    if (age >= 18) {
+        alert ("Bienvenue dans notre site");
+    } else {
+        alert ("Désolé vous n'ếtes pas autorisé à entrer");
+    }
+
+
+}
